477f06e6aca4eb0655b332430d0e54f8471cd9c3
sriramkv1409 <sriramkv1409@gmail.com>
2025-07-06 11:39:43 +0530

    feat:performed crud opertions

--- springbbootfirst/src/main/java/com/example/springbbootfirst/Controllers/HelloWorldController.java
+++ springbbootfirst/src/main/java/com/example/springbbootfirst/Controllers/HelloWorldController.java
@@ -8,35 +8,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class HelloWorldController {
     //Service layer
     @Autowired
     private HelloWorldService hws;
 
-//    @GetMapping("/hello")
-//    public String hello() {
-//       return hws.helloWorld();
-//    }
-//    @PostMapping("/")
-//    public String post(){
-//        return hws.postMethod();
-//    }
-//    @PutMapping("/")
-//    public String put(){
-//        return hws.putMethod();
-//    }
-//    @DeleteMapping("/")
-//    public String delete(){
-//        return hws.deleteMethod();
-//    }
-    @PostMapping("/employee")
+
+    @PostMapping
     public String addEmployee(@RequestBody Employee e){
         hws.addEmployee(e);
         return "Employee Added Successfully";
     }
 
-    @GetMapping("/employee")
+    @GetMapping
     public List<Employee> getEmployees(){
         return hws.getAllEmployees();
     }
+
+    @GetMapping("/{eid}")
+    public Employee getEmployeeById(@PathVariable int eid){
+        return hws.getEmployeeById(eid);
+    }
+
+    @DeleteMapping("/{eid}")
+    public String deleteEmployee(@PathVariable int eid){
+        return hws.deleteEmployeeById(eid);
+
+    }
+
+    @PutMapping("/{eid}")
+    public String updateRecord(@RequestBody Employee employee){
+        return hws.updateRecord(employee);
+    }
+
+
 }
--- springbbootfirst/src/main/java/com/example/springbbootfirst/Services/HelloWorldService.java
+++ springbbootfirst/src/main/java/com/example/springbbootfirst/Services/HelloWorldService.java
@@ -13,27 +13,60 @@ public class HelloWorldService {
             new Employee(1, "Prasanth", "Trainer"),
             new Employee(2, "Yuvaraj", "Faculty")
     ));
-
-//    public String helloWorld(){
-//        return "Hello World";
-//    }
-//
-//    public String postMethod(){
-//        return "This is Post Method";
-//    }
-//
-//    public String putMethod(){
-//        return "This is Put Method";
-//    }
-//
-//    public String deleteMethod(){
-//        return "This is Delete Method";
-//    }
 public void addEmployee(Employee e) {
     emp.add(e);
 }
 
     public List<Employee> getAllEmployees() {
         return emp;
+    }
+
+    public Employee getEmployeeById(int eid){
+          for(Employee e:emp){
+              if(e.getEid() == eid){
+                  return e;
+              }
+          }
+          return null;
+    }
+
+    public String deleteEmployeeById(int eid) {
+        int ind = 0;
+        boolean flag = false;
+        for(int i=0;i<emp.size();i++){
+            if(eid == emp.get(i).getEid()){
+                System.out.println("Emp_ID: "+ emp.get(i).getEid()+emp.get(i));
+                ind = i;
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            emp.remove(ind);
+            return "Deleted Employee Successfully!!!";
+        }
+        else{
+            return "Employee not found";
+        }
+    }
+
+    public String updateRecord(Employee e){
+        int ind = 0;
+        boolean flag = false;
+        for(int i=0;i<emp.size();i++){
+            if(e.getEid() == emp.get(i).getEid()){
+                System.out.println("Emp_ID: "+ emp.get(i).getEid()+emp.get(i));
+                ind = i;
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            emp.set(ind,e);
+            return "Updated Employee Successfully";
+        }
+        else{
+            return "Employee not found";
+        }
     }
 }
