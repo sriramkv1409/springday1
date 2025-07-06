6afa272dfa02bb4455f116118668e76e2a0b735a
sriramkv1409 <sriramkv1409@gmail.com>
2025-07-06 18:08:15 +0530

    FEAT:Added a new project

---
+++ todotask/src/test/java/com/example/todotask/TodotaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
